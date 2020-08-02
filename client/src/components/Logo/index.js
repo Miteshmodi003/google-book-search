bb32c17c8ff51f70f32a22e33c0b8c84589619e4
Mitesh Modi <mitesh.modi003@gmail.com>
2020-08-02 17:58:54 -0400

    google boo search

---
+++ client/src/components/Logo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css"
+
+function Logo() {
+    return (
+            <div className="logo">
+                <h1 className="logo-text logo-position google-book-search">Google Book Search</h1>
+                <h6 className="logo-text logo-position book-search-small">Built With React</h6>
+            </div>
+    )
+}
+
+export default Logo;
